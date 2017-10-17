13df9a05307438bb40500ceb02b7dda629cf4a20
David <davidfishe24@gmail.com>
2017-10-17 20:22:49 +0200

    Portfolio page layout added

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,20 @@
 window.onload = function(){
   $('#myCarousel').carousel({
-  interval: 10000
-})
+    interval: 10000
+  })
 
-$('.carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  }
-  else {
-    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
-});
+  $('.carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    if (next.next().length>0) {
+      next.next().children(':first-child').clone().appendTo($(this));
+    }
+    else {
+      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+  });
 }
