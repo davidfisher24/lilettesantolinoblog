bdfe110398ad203c77af457be7ff667921539559
David <davidfishe24@gmail.com>
2017-10-18 18:27:52 +0200

    Refactoring portfolio page files

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,10 @@
 window.onload = function(){
+
+  // CAROUSEL MODIFICATIONS
+
   $('#myCarousel').carousel({
     interval: 10000
-  })
+  });
 
   $('.carousel .item').each(function(){
     var next = $(this).next();
@@ -16,5 +19,12 @@ window.onload = function(){
     else {
       $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
     }
+  });
+
+  // GALLERY IMAGE MODIFICATIONS
+
+  $('.project-title').each(function(index,el){
+    var fontSize = parseInt($(el).css('font-size'))/2;
+    $(el).css({'margin-top' : '-' + fontSize + "px", 'opacity' : 1});
   });
 }
